d1933d980cce848049c293171ba099efcf77a51f
Sarah <sarah-almahfoudh@hotmail.com>
2021-09-10 16:36:59 +0300

    fix app case

--- models.py
+++ models.py
@@ -13,7 +13,7 @@ import json
 # database_path = "postgresql://{}:{}@{}/{}".format(
 #     username, password, url, database_name)
 database_path = 'postgres://kvwxtgpckchsfh:8a8b32b7bbef5a242adadadaff33ff19cdd3629fb8842795210efb0c4d0da2ee@ec2-44-195-16-34.compute-1.amazonaws.com:5432/d8nbles1ii1qdg'
-database_path = os.environ['DATABASE_URL']
+# database_path = os.environ['DATABASE_URL']
 db = SQLAlchemy()
 
 
